36eef003ac00ea4f242f23095a222afcc4343459
abhijeeetdewtarse <83063553+abhijeeetdewtarse@users.noreply.github.com>
2026-02-11 16:52:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function insert_Row() {
-    //Write your code here
-  
-  
+let table= document.getElementById('sampleTable');
+	let newrow = table.insertRow();
+	let cell1 = newrow.insertCell(0);
+	let cell2 = newrow.insertCell(1);
+	cell1.textContent = 'new cell1';
+	cell2.textContent = 'new cell2';
 }
